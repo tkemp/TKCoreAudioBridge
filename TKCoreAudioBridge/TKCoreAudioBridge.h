09281d6ef049d5200c62319cc2e8acd6a1a69d22
Tim Kemp <timothykemp@gmail.com>
2012-12-12 10:18:55 -0500

    Initial Commit

---
+++ TKCoreAudioBridge/TKCoreAudioBridge.h
@@ -0,0 +1,13 @@
+//
+//  TKCoreAudioBridge.h
+//  TKCoreAudioBridge
+//
+//  Created by Tim Kemp on 12/12/2012.
+//  Copyright (c) 2012 Tim Kemp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TKCoreAudioBridge : NSObject
+
+@end
